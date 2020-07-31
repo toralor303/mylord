c3bb6307bfa1cd1ee55fce42523db940d720bbcc
Charles Sorel <Toralor@users.noreply.github.com>
2020-07-31 18:53:26 -0400

    Updates

---
+++ precache-manifest.6d1644a2365132a7d1d6dace8cc84c6b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8572c0e00d3a5109ad9fed1f7796461",
+    "url": "/mylord/index.html"
+  },
+  {
+    "revision": "c9b9d56780a993538701",
+    "url": "/mylord/static/css/main.f8ab94b4.chunk.css"
+  },
+  {
+    "revision": "645c727f269155ffa5d2",
+    "url": "/mylord/static/js/2.47d9373f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/mylord/static/js/2.47d9373f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c9b9d56780a993538701",
+    "url": "/mylord/static/js/main.0122c293.chunk.js"
+  },
+  {
+    "revision": "0c3fc98b357492c0f749",
+    "url": "/mylord/static/js/runtime-main.e81e822a.js"
+  }
+]);
